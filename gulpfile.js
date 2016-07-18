2cdd781a72ea4c8a4b2cd450d4000ba52669e97f
i5ting <i5ting@126.com>
2016-07-18 15:35:45 +0800

    lazycommmit, please ignore

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var gulp = require('gulp')
 var gp_deploy = require('gulp-gh-pages')
 var open = require("gulp-open")
 var rename = require("gulp-rename")
-
 var options = {}
 
 gulp.task('deploy', function () {
@@ -12,7 +11,7 @@ gulp.task('deploy', function () {
     .pipe(gp_deploy(options))
 })
 
-gulp.task('generate',function () {
+gulp.task('generate', function () {
 	// Run external tool synchronously
 	if (exec('npm run build').code !== 0) {
 	  echo('Error: generate.sh exec failed')
@@ -20,7 +19,7 @@ gulp.task('generate',function () {
 	}	
 })
 
-gulp.task('show',['generate'] ,function () {
+gulp.task('show',['generate'] , function () {
   if (exec('hs _book -o').code !== 0) {
 	  echo('Error: generate.sh exec failed')
 	  exit(1)
@@ -29,6 +28,6 @@ gulp.task('show',['generate'] ,function () {
   console.log('show')
 })
 
-gulp.task('default',['generate', 'rename', 'deploy'] ,function () {
+gulp.task('default', ['generate', 'deploy'] , function () {
   console.log('default')
 })
